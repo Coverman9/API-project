9516cc1a6425fe3e5e8b0eca8cf42f4fc50528a5
Coverman9 <usubalieve@gmail.com>
2023-03-16 14:56:34 -0500

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,14 @@
 // backend/routes/api/index.js
-const router = require('express').Router();
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+router.use(restoreUser);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
+
+// GET /api/set-token-cookie
 
 
 
