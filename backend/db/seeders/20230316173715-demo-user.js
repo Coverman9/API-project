49538abff18cb402ffd6022f6bab75a8033794ba
Coverman9 <usubalieve@gmail.com>
2023-03-16 13:12:55 -0500

    commit check again

--- backend/db/seeders/20230316173715-demo-user.js
+++ backend/db/seeders/20230316173715-demo-user.js
@@ -2,7 +2,8 @@
 const bcrypt = require("bcryptjs")
 let options = {}
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+  options.schema = process.env.SCHEMA;
+  // define your schema in options object
 }
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
