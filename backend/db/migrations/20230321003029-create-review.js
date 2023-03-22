f70eb935524414ffcc912291451fc4c32a388b98
Coverman9 <usubalieve@gmail.com>
2023-03-22 11:02:43 -0500

    fixed migrations

--- backend/db/migrations/20230321003029-create-review.js
+++ backend/db/migrations/20230321003029-create-review.js
@@ -42,7 +42,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: new Date()
       },
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Reviews";
