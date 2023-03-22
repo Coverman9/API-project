c22b256f942057753e03cc46fd19b0395f12bc3a
Coverman9 <usubalieve@gmail.com>
2023-03-22 14:58:41 -0500

    reviews enpoints

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -14,32 +14,69 @@ const { Op } = require("sequelize");
 
 const router = express.Router();
 
-router.get('/current', requireAuth, async(req, res) => {
-    const { user } = req
-    const review = await Review.findAll({
-        where: {
-            userId: user.id
+//Get all Reviews of the Current User
+router.get("/current", requireAuth, async (req, res) => {
+    const { user } = req;
+    const reviews = await Review.findAll({
+      where: {
+        userId: user.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["id", "firstName", "lastName"],
         },
-        include: [
-            {
-                model: User,
-                attributes: ["id", "firstName", "lastName"]
-            },
-            {
-                model: Spot,
-            },
-            {
-                model: ReviewImage,
-                attributes: ["id", "url"]
-            }
-        ]
+        {
+          model: Spot,
+          attributes: [
+            "id",
+            "ownerId",
+            "address",
+            "city",
+            "state",
+            "country",
+            "lat",
+            "lng",
+            "name",
+            "price",
+          ],
+          include: [{ model: SpotImage, attributes: ["url", "preview"] }],
+        },
+        {
+          model: ReviewImage,
+          attributes: ["id", "url"],
+        },
+      ],
+    });
+    const reviewObjects = [];
+    if (reviews.length) {
+      reviews.forEach((review) => reviewObjects.push(review.toJSON()));
+    } else {
+      reviewObjects.push(reviews);
+    }
+    for (let review of reviewObjects) {
+      if (!Object.keys(review).length) break
+      if (review.Spot.SpotImages.length) {
+          const filterTrue = review.Spot.SpotImages.filter(
+              image => image.preview === true
+          )
+          if (filterTrue.length) {
+              review.Spot.previewImage = filterTrue[0].url
+          } else {
+              review.Spot.previewImage = "No Preview Image Available"
+          }
+      } else {
+          review.Spot.previewImage = "No Preview Image Available"
+      }
+      delete review.Spot.SpotImages
 
-    })
+
+    }
 
     res.json({
-        Reviews: review
-    })
-})
+      Reviews: reviewObjects,
+    });
+  });
 
 
 
