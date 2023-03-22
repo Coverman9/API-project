7d6c9dae3f4a4712255c592d93214deb9f431979
Coverman9 <usubalieve@gmail.com>
2023-03-22 18:30:06 -0500

    reviews edit and new

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -77,5 +77,65 @@ router.get("/current", requireAuth, async (req, res) => {
 });
 
 
+//Add an Image to a Review based on the Review's id
+router.post("/:reviewId/images", requireAuth, async (req, res, next) => {
+  const { url } = req.body;
+  const reviewId = parseInt(req.params.reviewId);
+
+  const reviews = await Review.findAll();
+  const reviewsArr = [];
+  let isExist = false;
+  reviews.forEach((review) => reviewsArr.push(review.toJSON()));
+  for (let review of reviewsArr) {
+    if (review.id == reviewId) {
+      isExist = true;
+      break;
+    }
+  }
+  if (!isExist) {
+    const err = new Error("Review couldn't be found");
+    res.status(404);
+    return next(err);
+  }
+  const reviewImages = await ReviewImage.findAll({
+    where: {
+        reviewId
+    }
+  })
+  if (reviewImages.length > 10) {
+    const err = new Error("Maximum number of images for this resource was reached")
+    res.status(404)
+    return next(err)
+}
+  const newImage = await ReviewImage.create({
+    reviewId,
+    url,
+  });
+  res.json(newImage);
+});
+
+
+//Edit a Review
+router.put('/:reviewId', requireAuth, async(req, res, next) => {
+    const { user } = req
+    const { review, stars } = req.body
+    const updatedReview = await Review.findByPk(req.params.reviewId)
+    if (!updatedReview) {
+        const err = new Error("Review couldn't be found")
+        res.status(404)
+        return next(err)
+    }
+    if(updatedReview.toJSON().userId == user.id) {
+        await updatedReview.update({
+            review,
+            stars
+        })
+    } else {
+        const err = new Error("Can edit only your reviews")
+        res.status(404)
+        return next(err)
+    }
+    res.json(updatedReview)
+})
 
 module.exports = router;
